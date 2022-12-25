af719697dc1b788da24bb910d6c8986c5450ee2b
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-12-25 02:52:34 -0500

    adding function to show amenities

--- web_dynamic/static/scripts/101-hbnb.js
+++ web_dynamic/static/scripts/101-hbnb.js
@@ -105,6 +105,40 @@ window.onload = function () {
     }
   });
   
+  $(function () {
+    const amenitiesIds = [];
+    const statesIds = [];
+    const citiesIds = [];
+    $('.amenities .popover input').change(function (e) {
+      const { target } = e;
+      const id = target.getAttribute('data-id');
+      const isChecked = $(`.popover input[data-id="${id}"]`).is(':checked');
+  
+      if (isChecked) {
+        amenitiesIds.push(id);
+      } else {
+        const index = amenitiesIds.indexOf(id);
+        if (index > -1) amenitiesIds.splice(index, 1);
+      }
+  
+      const entertainment = [];
+      for (let i = 0; i < amenitiesIds.length; i++) {
+        const name = $(`.popover input[data-id="${amenitiesIds[i]}"]`).attr(
+          'data-name'
+        );
+        entertainment.push(name);
+      }
+  
+      if (entertainment.length > 0) {
+        const newText = entertainment.join(', ');
+        newText.length > 32
+          ? $('.amenities h4').text(`${newText.slice(0, 28)}...`)
+          : $('.amenities h4').text(newText);
+      } else {
+        $('.amenities h4').html('&nbsp;');
+      }
+    });
+
   $('section.filters button').click(function () {
 //    debugger
     const amnts = Object.keys(amenities)
