34125ae76fcf29ca5bd05d5acaf145c74688d7db
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-12-25 02:57:54 -0500

    deleting function to list amenities

--- web_dynamic/static/scripts/101-hbnb.js
+++ web_dynamic/static/scripts/101-hbnb.js
@@ -85,7 +85,9 @@ window.onload = function () {
 	);
       });
     });
+
   };
+
   $(document).on('click', '.handle-reviews', function () {
 //    debugger
     console.log(this)
@@ -104,40 +106,6 @@ window.onload = function () {
       $(this).addClass('used');
     }
   });
-  
-  $(function () {
-    const amenitiesIds = [];
-    const statesIds = [];
-    const citiesIds = [];
-    $('.amenities .popover input').change(function (e) {
-      const { target } = e;
-      const id = target.getAttribute('data-id');
-      const isChecked = $(`.popover input[data-id="${id}"]`).is(':checked');
-  
-      if (isChecked) {
-        amenitiesIds.push(id);
-      } else {
-        const index = amenitiesIds.indexOf(id);
-        if (index > -1) amenitiesIds.splice(index, 1);
-      }
-  
-      const entertainment = [];
-      for (let i = 0; i < amenitiesIds.length; i++) {
-        const name = $(`.popover input[data-id="${amenitiesIds[i]}"]`).attr(
-          'data-name'
-        );
-        entertainment.push(name);
-      }
-  
-      if (entertainment.length > 0) {
-        const newText = entertainment.join(', ');
-        newText.length > 32
-          ? $('.amenities h4').text(`${newText.slice(0, 28)}...`)
-          : $('.amenities h4').text(newText);
-      } else {
-        $('.amenities h4').html('&nbsp;');
-      }
-    });
 
   $('section.filters button').click(function () {
 //    debugger
